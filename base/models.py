da520c04c94815dd93a3ef1aecfdbc8440d5f150
hshinf <hshinf@gmail.com>
2016-07-12 17:47:19 -0400

    model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Business(models.Model):
+    business_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100)
+    owner = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=1)
+   
+class Coupon(models.Model):
+    title = models.CharField(max_length=50)
+    business = models.CharField(max_length=50)
+    about = models.CharField(max_length=100)
+    expire_date = models.CharField(max_length=20)
+    is_void = models.BooleanField(default=0)
+    
