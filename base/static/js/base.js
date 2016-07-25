6f606326eb99729d0d5ddec9c4fc53af87621c82
timebigot <timebigot@gmail.com>
2016-07-25 17:02:35 -0400

    Add share page and segregate coupon modal html

--- base/static/js/base.js
+++ base/static/js/base.js
@@ -1,8 +1,10 @@
 $('#couponModal').on('show.bs.modal', function (event) {
-    var anchor = $(event.relatedTarget) // Button that triggered the modal
-    var title = anchor.data('title')
-    var bizname = anchor.data('bizname')
-    var modal = $(this)
-    modal.find('.coupon-title').text(title)
-    modal.find('.coupon-bizname').text(bizname)
+    var anchor = $(event.relatedTarget);
+    var title = anchor.data('title');
+    var bizname = anchor.data('bizname');
+    var urlcode = anchor.data('urlcode');
+    var modal = $(this);
+    modal.find('.coupon-title').text(title);
+    modal.find('.coupon-bizname').text(bizname);
+    modal.find('.coupon-urlcode').val('http://dcjura.com/c/' + urlcode)
 })
