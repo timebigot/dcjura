93c60bd6bc83f5b09f56f1ea104f88467b9c2fa5
timebigot <timebigot@gmail.com>
2016-08-05 00:08:28 +0000

    Final media image commit from server

--- aws_storages.py
+++ aws_storages.py
@@ -1,5 +1,5 @@
-from django.conf import settings as base_settings
+from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
 
 class MediaStorage(S3BotoStorage):
-    location = base_settings.MEDIAFILES_LOCATION
+    location = settings.MEDIA_LOCATION
